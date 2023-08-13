48f543f4c9f422deca238e81788c3122ce00d7d5
nikita zigman <zigman.nikita@gmail.com>
2023-08-13 20:58:32 +0100

    feat: updates endpointes with the latest changes

--- backend/services/gitbridge/src/api/v1/github.py
+++ backend/services/gitbridge/src/api/v1/github.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.schemas.responses import BaseResponse
 from src.schemas.issues import GithubIssuePayload
 from fastapi.requests import Request
 from loguru import logger
+from src.services.github import get_github_issue_webhook_service, IGitHubWebhookService
+from typing import Annotated
 
 router = APIRouter()
 
@@ -13,11 +15,15 @@ router = APIRouter()
     summary="Github issue webhook handler",
     description="bridge between github issue webhook and a rabbitmq git queue",
 )
-async def issue_handler(request: Request, payload: GithubIssuePayload):
-    headers = request.headers
-    logger.info(headers)
-    logger.info(payload)
-    return BaseResponse()
+async def issue_handler(
+    request: Request,
+    payload: GithubIssuePayload,
+    service: Annotated[
+        IGitHubWebhookService[GithubIssuePayload],
+        Depends(get_github_issue_webhook_service),
+    ],
+):
+    return await service.handle_webhook_payload(secret_key="mock key", payload=payload)
 
 
 @router.post(
